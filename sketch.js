708c1124ff0a6e7e7e46b1e049fe2af974a8b716
ad276 <85683804+ad276@users.noreply.github.com>
2021-06-19 00:50:17 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,47 @@
+var ship,ship_running;
+var sea;
+
+var sea,sea_img;
 
 function preload(){
+  ship_running = loadAnimation("trex1.png","trex3.png","trex4.png");
+sea_img = loadImage("ground2.png")
+}
+
+function setup()
+{
+createCanvas(600,200);
+
+ship = createSprite(50,160,20,50);
+ship.addAnimation("running",ship_running);
+
+
+ship.scale = 0.5;
+
+sea = createSprite(200,180,600,20);
+sea.addImage(ground_img)
+
 
 }
 
-function setup(){
-  createCanvas(400,400);
-  
+function draw(){
+  background("white");
+  sea.velocityX = -1;
+
+  if(sea<0){
+    sea.x = sea.width/2;
+  }
+
+if(keyDown("space")){
+  ship.velocityY = -10;
 }
 
-function draw() {
-  background("blue");
- 
+ship.velocityY = ship.velocityY + 0.5;
+ship.collide(sea);
+
+
+
+
+  drawSprites();
 }
+
